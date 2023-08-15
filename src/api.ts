765f9635135c1e1e3e29e381c68707e7f3c5a473
Darwin <charmful0x@gmail.com>
2023-08-15 17:47:26 +0300

    chore: update logs naming

--- src/api.ts
+++ src/api.ts
@@ -1,11 +1,11 @@
 import express, { Express, Request, Response } from "express";
-import { resolveHackerNoonUser } from "./molecules/hackernoon/atom";
+import { resolveHackerNoonUser } from "./molecules/hackernoon/atom"; 
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("⚛️ molecule-2 ⚛️");
+  res.send("⚛️ molecule-ext-1 ⚛️");
 });
 
 app.get("/resolve-hn/:address", async (req: Request, res: Response) => {
@@ -15,5 +15,5 @@ app.get("/resolve-hn/:address", async (req: Request, res: Response) => {
 });
 
 app.listen(port, () => {
-  console.log(`[molecule-2] running on port: ${port}`);
+  console.log(`[molecule-ext-1] running on port: ${port}`);
 });
