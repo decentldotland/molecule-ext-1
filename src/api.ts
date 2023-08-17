45fdc67e716b3bdd14cc8f80deb6e3da6e1a077b
Darwin <charmful0x@gmail.com>
2023-08-17 13:43:31 +0300

    feat: endpoint `/karma3labs/score/`

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,7 @@
 import express, { Express, Request, Response } from "express";
-import { resolveHackerNoonUser } from "./molecules/hackernoon/atom"; 
+import { resolveHackerNoonUser } from "./molecules/hackernoon/atom";
+import { getKarmaScore } from "./molecules/karma3labs/atom";
+import type { Strategy } from "@/types";
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
@@ -13,6 +15,19 @@ app.get("/hackernoon/resolve/:address", async (req: Request, res: Response) => {
   const profile = await resolveHackerNoonUser(address);
   res.send(profile);
 });
+
+app.get(
+  "/karma3labs/score/:handle/:strategy",
+  async (req: Request, res: Response) => {
+    const { handle, strategy } = req.params;
+    const options = {
+      handle: handle,
+      strategy: strategy as Strategy,
+    };
+    const profile = await getKarmaScore(options);
+    res.send(profile);
+  },
+);
 
 app.listen(port, () => {
   console.log(`[molecule-ext-1] running on port: ${port}`);
