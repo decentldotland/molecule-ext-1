b2d340412aaa39fc3b23c8914ce7d61fc764f015
Darwin <charmful0x@gmail.com>
2023-08-21 14:37:49 +0300

    feat: `/balance` endpoints

--- src/api.ts
+++ src/api.ts
@@ -2,6 +2,10 @@ import express, { Express, Request, Response } from "express";
 import { resolveHackerNoonUser } from "./molecules/hackernoon/atom";
 import { getKarmaScore } from "./molecules/karma3labs/atom";
 import { getArseedOwner } from "./molecules/arseed/atom";
+import {
+  moralisTokenBalance,
+  moralisNftBalance,
+} from "./molecules/balances/atom";
 import type { Strategy } from "@/types";
 
 const app: Express = express();
@@ -36,6 +40,37 @@ app.get("/arseed/verify/:txid/:pubkey", async (req: Request, res: Response) => {
   const result = await getArseedOwner(txid, pubkey);
   res.send(result);
 });
+
+app.get(
+  "/balance/token/:chain/:address/:contract",
+  async (req: Request, res: Response) => {
+    const { chain, address, contract } = req.params;
+
+    if (!["eth", "goerli", "polygon", "bsc", "fantom"].includes(chain)) {
+      res.send({ error: "invalid chain key supplied" });
+      return;
+    }
+
+    const result = await moralisTokenBalance(address, contract, chain);
+    res.send(result);
+    return;
+  },
+);
+
+app.get(
+  "/balance/nft/:chain/:address/:contract",
+  async (req: Request, res: Response) => {
+    const { chain, address, contract } = req.params;
+
+    if (!["eth", "goerli", "polygon", "bsc", "fantom"].includes(chain)) {
+      res.send({ error: "invalid chain key supplied" });
+      return;
+    }
+    const result = await moralisNftBalance(address, contract, chain);
+    res.send(result);
+    return;
+  },
+);
 
 app.listen(port, () => {
   console.log(`[molecule-ext-1] running on port: ${port}`);
